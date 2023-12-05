b0a728a1093a4c668510bfb7833e82c4f1b26068
arianacatani2009 <mayerlicatani@gmail.com>
2023-12-05 15:49:13 -0500

    web server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+/*
 //creacion de un web server en express
-const express = require('express')
+//const express = require('express')
 const {engine} = require('express-handlebars')
 //invocar la libreria de express
 //crear una instancia
-const app = express()
+//const app = express()
 app.engine('handlebars',engine);
 app.set('view engine','handlebars');
 //porsicaso no funcione
@@ -15,6 +16,37 @@ app.get('/hamburguesa/vegana',(req,res)=>{
 })
 app.get('/hamburguesa/contactos',(req,res)=>{
     res.render('contactos')
+})
+*/
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+/*
+app.get('/',(req,res)=>{
+    //request de la peticion -  cliente
+    req.body
+    req.params
+    req.query
+})
+*/
+//publicas
+app.get('/entrada',(req,res)=>{
+    res.send("Entrada al local")
+})
+//crear un middleware
+app.use((req,res,next)=>{
+    const{email,password} = req.body
+    if(email === "arianacatani@gmail.com" && password === "12345"){
+        next()
+    }
+    else{
+        res.send("Usuario no registrado")
+    }
+})
+//privadas
+app.get('/pedido',(req,res)=>{
+    res.send("Bienvenido -- listo para tomar su orden")
 })
 
 //ruta
